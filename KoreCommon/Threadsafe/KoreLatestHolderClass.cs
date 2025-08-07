287ed94b5cfc520d8b03519b0448f010fbff5040
Dave Steadman <davesteadman@me.com>
2025-08-07 23:57:23 +0100

    Updates matching consumer development

--- KoreCommon/Threadsafe/KoreLatestHolderClass.cs
+++ KoreCommon/Threadsafe/KoreLatestHolderClass.cs
@@ -1,4 +1,4 @@
-// KoreLatestHolder:
+// KoreLatestHolderClass:
 // - A simple thread-safe holder for a single value of type T. It uses a lock to ensure that the latest value can be safely updated and read from multiple threads.
 // - Use Case: The background thread updates the value, and the main thread gets the latest value regardless of when it was last updated.
 
@@ -9,11 +9,11 @@ using System.Threading;
 
 #nullable enable
 
-public class KoreLatestHolder<T> where T : class
+public class KoreLatestHolderClass<T> where T : class
 {
     private T? _latestValue;
 
-    public KoreLatestHolder(T? initialValue = null)
+    public KoreLatestHolderClass(T? initialValue = null)
     {
         _latestValue = initialValue;
     }
